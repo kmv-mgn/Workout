4d33fd11e131f5f0c25e449cf319769f287b3326
kmv <kmv_mgn@mail.ru>
2016-11-30 19:57:08 +0500

    telefone and planshet

--- app/src/main/java/com/hfad/workout/MainActivity.java
+++ app/src/main/java/com/hfad/workout/MainActivity.java
@@ -1,19 +1,45 @@
 package com.hfad.workout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements WorkoutListFragment.WorkoutListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*   //прямое задание значений фрагменту
         WorkoutDetailFragment frag = (WorkoutDetailFragment)
                 getFragmentManager().findFragmentById(R.id.detail_frag);
-        frag.setWorkout(1);
+        frag.setWorkout(1);*/
+
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        //Здесь размещается код отображения подробной информаици
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if (fragmentContainer != null) {
+            //если фрагмент fragment_container существует, занчит используется макет для планшетов
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        } else {   //фрагмент не найден, значит используется макет для телефонов
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
+
 
     }
 }
--- app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.hfad.workout.R;
 
@@ -24,8 +25,29 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong("workoutId", workoutId);
     }
 
     public void setWorkout(long id) {
